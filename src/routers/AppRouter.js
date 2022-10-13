d8502bfd6e1f2eed8a8b386a702099116239bd4e
Daniel312006 <pio.perez@est.iudigital.edu.co>
2022-10-13 15:47:54 -0500

    correcciones finales

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import NavBar from "../components/ui/NavBar"
+import Container from 'react-bootstrap/Container';
+import { Navbar, Nav } from 'react-bootstrap';
 import Home from "../components/ui/Home"
 import Estados from "../components/estados/Estados"
 import Marcas from "../components/marcas/Marcas"
@@ -12,7 +13,21 @@ import NotFound from "../components/ui/NotFound"
 export default function AppRouter() {
     return (
         <div>
-            <NavBar title={'IE EL ROSARIO'} />
+            <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">IE EL ROSARIO</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/inventarios">Inventario</Nav.Link>
+                    <Nav.Link href="/Estados">Estados</Nav.Link>
+                    <Nav.Link href="/Marcas">Marcas</Nav.Link>
+                    <Nav.Link href="/Tipos">Tipos</Nav.Link>
+                    <Nav.Link href="/Usuarios">Usuarios</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
             <main className="container">
                 <Routes>
                     <Route path="/" element={<Home />} />
