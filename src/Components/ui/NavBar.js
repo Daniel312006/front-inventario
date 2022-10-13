b60a417348f230d2a60d4074d3a97a0acd2f664c
Daniel312006 <pio.perez@est.iudigital.edu.co>
2022-10-13 15:36:35 -0500

    correccion navbar

--- src/Components/ui/NavBar.js
+++ src/Components/ui/NavBar.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-const Barra = () => {
+const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -23,4 +23,4 @@ const Barra = () => {
   );
 }
 
-export default Barra
+export default NavBar
